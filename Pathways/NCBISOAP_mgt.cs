ac0e6e7599ef66756e630a132bc14d76339a9e1d
ArxCaeli <levaya@bk.ru>
2013-05-21 10:49:57 +0400

    DLL compile issues commented

--- Pathways/NCBISOAP_mgt.cs
+++ Pathways/NCBISOAP_mgt.cs
@@ -8,75 +8,75 @@ namespace Pathways
 {
 	public static class NCBISOAP_mgt
 	{
-		public static List<string> GetFastaByAccNo(List<string> NCBI_GI, int FastaLimit)
-		{
-			List<string> FastaSequences = new List<string>();
+//        public static List<string> GetFastaByAccNo(List<string> NCBI_GI, int FastaLimit)
+//        {
+//            List<string> FastaSequences = new List<string>();
 
-			eFetchSequenceService Serv = new eFetchSequenceService();
-			MessageEFetchRequest Message = new MessageEFetchRequest();
+//            eFetchSequenceService Serv = new eFetchSequenceService();
+//            MessageEFetchRequest Message = new MessageEFetchRequest();
 
-			Message.db = "gene";///"nuccore";
-			Message.id = NCBI_GI[0];//ComposeRequest(NCBI_GI);
-			//Message.strand = "1";
-			//Message.retmax = "10"; 
-//			Message.seq_start = "1";
-//			Message.seq_stop = FastaLimit.ToString(); // not working :/
-//			Message.rettype = "fasta"; 
-//			Message.complexity = "0"; // minimum xxx
-//
-			MessageEFetchResult Res = Serv.run_eFetch(Message);
+//            Message.db = "gene";///"nuccore";
+//            Message.id = NCBI_GI[0];//ComposeRequest(NCBI_GI);
+//            //Message.strand = "1";
+//            //Message.retmax = "10"; 
+////			Message.seq_start = "1";
+////			Message.seq_stop = FastaLimit.ToString(); // not working :/
+////			Message.rettype = "fasta"; 
+////			Message.complexity = "0"; // minimum xxx
+////
+//            MessageEFetchResult Res = Serv.run_eFetch(Message);
 
-			foreach(TSeq Seq in Res.TSeqSet)
-			{
-				if (Seq.TSeq_seqtype.value.ToString() == "nucleotide")
-					FastaSequences.Add(ComposeFasta(Seq, FastaLimit));
-			}
+//            foreach(TSeq Seq in Res.TSeqSet)
+//            {
+//                if (Seq.TSeq_seqtype.value.ToString() == "nucleotide")
+//                    FastaSequences.Add(ComposeFasta(Seq, FastaLimit));
+//            }
 
-			return FastaSequences;
-		}
+//            return FastaSequences;
+//        }
 
-//		public static List<string> GetFastaByGenes(List<string> NCBI_GI, int FastaLimit)
-//		{
-//		
-//			List<string> FastaSequences = new List<string>();
-//
-//			eFetchGeneService Serv = new eFetchGeneService();
-//
-//
-//			MessageEFetchRequest Message = new MessageEFetchRequest();
-//
-//			Message.id = NCBI_GI[0];//ComposeRequest(NCBI_GI);
-//
-//
-//			MessageEFetchResult Res = Serv.run_eFetch(Message);
-//
-////			foreach(TSeq Seq in Res.TSeqSet)
-////			{
-////				if (Seq.TSeq_seqtype.value.ToString() == "nucleotide")
-////					FastaSequences.Add(ComposeFasta(Seq, FastaLimit));
-////			}
-//
-//			return FastaSequences;
-//		}
+////		public static List<string> GetFastaByGenes(List<string> NCBI_GI, int FastaLimit)
+////		{
+////		
+////			List<string> FastaSequences = new List<string>();
+////
+////			eFetchGeneService Serv = new eFetchGeneService();
+////
+////
+////			MessageEFetchRequest Message = new MessageEFetchRequest();
+////
+////			Message.id = NCBI_GI[0];//ComposeRequest(NCBI_GI);
+////
+////
+////			MessageEFetchResult Res = Serv.run_eFetch(Message);
+////
+//////			foreach(TSeq Seq in Res.TSeqSet)
+//////			{
+//////				if (Seq.TSeq_seqtype.value.ToString() == "nucleotide")
+//////					FastaSequences.Add(ComposeFasta(Seq, FastaLimit));
+//////			}
+////
+////			return FastaSequences;
+////		}
 
-		private static string ComposeFasta (TSeq Seq, int FastaLimit)
-		{
-			return ">gi|" + Seq.TSeq_gi + "|gb|" + Seq.TSeq_accver + // gb - wrong
-				"| " + Seq.TSeq_defline + "\n" + Seq.TSeq_sequence.Substring(0,FastaLimit);
-		}
+//        private static string ComposeFasta (TSeq Seq, int FastaLimit)
+//        {
+//            return ">gi|" + Seq.TSeq_gi + "|gb|" + Seq.TSeq_accver + // gb - wrong
+//                "| " + Seq.TSeq_defline + "\n" + Seq.TSeq_sequence.Substring(0,FastaLimit);
+//        }
 
-		public static string ComposeRequest(List<string> Entries)
-		{
-			string Request = "";
+//        public static string ComposeRequest(List<string> Entries)
+//        {
+//            string Request = "";
 
-			foreach(string Entry in Entries)
-			{
-				if (Request != "")
-					Request += ",";
-				Request += Entry;
-			}
-			return Request;
-		}
+//            foreach(string Entry in Entries)
+//            {
+//                if (Request != "")
+//                    Request += ",";
+//                Request += Entry;
+//            }
+//            return Request;
+//        }
 	}
 }
 
--- Pathways/NCBI_HTTP.cs
+++ Pathways/NCBI_HTTP.cs
@@ -11,72 +11,72 @@ namespace Pathways
 {
 	public static class NCBI_HTTP
 	{
-		//http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=312836839,34577063&rettype=fasta&retmode=text
-		public static string GetFastaByGenes (List<string> NCBI_GI)
-		{
-			//List<string> FastaSequences = new List<string> ();
+        ////http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=312836839,34577063&rettype=fasta&retmode=text
+        //public static string GetFastaByGenes (List<string> NCBI_GI)
+        //{
+        //    //List<string> FastaSequences = new List<string> ();
 
-			return HTTP_Mgt.HttpGet(FetchSequenceURL(NCBI_GI));
-		}
+        //    return HTTP_Mgt.HttpGet(FetchSequenceURL(NCBI_GI));
+        //}
 
-		public static string GetGeneInfoXML (string NCBI_GeneId)
-		{
-			return HTTP_Mgt.HttpGet(FetchGeneInfoXMLURL(NCBI_GeneId));
-		}
+        //public static string GetGeneInfoXML (string NCBI_GeneId)
+        //{
+        //    return HTTP_Mgt.HttpGet(FetchGeneInfoXMLURL(NCBI_GeneId));
+        //}
 
-		private static string FetchSequenceURL(List<string> NCBI_GI)
-		{
-			//http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=312836839,34577063&rettype=fasta&retmode=text
+        //private static string FetchSequenceURL(List<string> NCBI_GI)
+        //{
+        //    //http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=312836839,34577063&rettype=fasta&retmode=text
 
-			// for protein:
-			//return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=" + 
-			// for mRna
-			//return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=" +	
+        //    // for protein:
+        //    //return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sequences&id=" + 
+        //    // for mRna
+        //    //return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=" +	
 			
-			return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=" +			
-				NCBISOAP_mgt.ComposeRequest(NCBI_GI) + @"&rettype=fasta&retmode=text";
-		}
+        //    return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=" +			
+        //        NCBISOAP_mgt.ComposeRequest(NCBI_GI) + @"&rettype=fasta&retmode=text";
+        //}
 
-		private static string FetchGeneInfoXMLURL (string GeneID)
-		{
-			//http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=2&retmode=xml
-			return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=" + 
-				GeneID + "&retmode=xml";
-		}
+        //private static string FetchGeneInfoXMLURL (string GeneID)
+        //{
+        //    //http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=2&retmode=xml
+        //    return @"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id=" + 
+        //        GeneID + "&retmode=xml";
+        //}
 
-		public static List<string> SplitFasta (string Fasta, int FastaLimit)
-		{
-			List<string> FastaSeq = new List<string>();
+        //public static List<string> SplitFasta (string Fasta, int FastaLimit)
+        //{
+        //    List<string> FastaSeq = new List<string>();
 
-			while(Fasta.Contains(">"))
-			{
-				//to do: not working
-				Fasta = Fasta.Remove(0, Fasta.IndexOf(">"));
+        //    while(Fasta.Contains(">"))
+        //    {
+        //        //to do: not working
+        //        Fasta = Fasta.Remove(0, Fasta.IndexOf(">"));
 				
-				string Descr = Fasta.Substring(0, Fasta.IndexOf("\n"));
-				Fasta = Fasta.Remove(0, Fasta.IndexOf("\n") + 1);
-				string FastaData = Fasta.Substring(0, Fasta.IndexOf(">"));
+        //        string Descr = Fasta.Substring(0, Fasta.IndexOf("\n"));
+        //        Fasta = Fasta.Remove(0, Fasta.IndexOf("\n") + 1);
+        //        string FastaData = Fasta.Substring(0, Fasta.IndexOf(">"));
 
-				FastaData = FastaData.Substring(0, Math.Min(FastaLimit, FastaData.Length));
+        //        FastaData = FastaData.Substring(0, Math.Min(FastaLimit, FastaData.Length));
 
-				FastaSeq.Add(Descr + "\n" + FastaData);
-			}
+        //        FastaSeq.Add(Descr + "\n" + FastaData);
+        //    }
 
-			return FastaSeq;
-		}
+        //    return FastaSeq;
+        //}
 
-		public static string GetGeneAccessionNo (string XMLGeneInfo)
-		{
-			MemoryStream stream = new MemoryStream();
-			StreamWriter writer = new StreamWriter(stream);
-			writer.Write(XMLGeneInfo);
-			writer.Flush();
+        //public static string GetGeneAccessionNo (string XMLGeneInfo)
+        //{
+        //    MemoryStream stream = new MemoryStream();
+        //    StreamWriter writer = new StreamWriter(stream);
+        //    writer.Write(XMLGeneInfo);
+        //    writer.Flush();
 
-			// to do:http://www.biostars.org/p/375/
-			XDocument Doc = XDocument.Load(stream);
-			// get 
-			return "";
-		}
+        //    // to do:http://www.biostars.org/p/375/
+        //    XDocument Doc = XDocument.Load(stream);
+        //    // get 
+        //    return "";
+        //}
 	}
 }
 
